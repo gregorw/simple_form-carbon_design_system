b60302f5896bf1cfcdb9357dfe6b56e75d3da4c6
Gregor Wassmann <gregor.wassmann@gmail.com>
2022-11-22 22:09:37 +0100

    Verify test setup and CI

--- test/simple_form/test_carbon_design_system.rb
+++ test/simple_form/test_carbon_design_system.rb
@@ -2,12 +2,14 @@
 
 require "test_helper"
 
-class SimpleForm::TestCarbonDesignSystem < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::SimpleForm::CarbonDesignSystem::VERSION
-  end
+module SimpleForm
+  class TestCarbonDesignSystem < Minitest::Test
+    def test_that_it_has_a_version_number
+      refute_nil ::SimpleForm::CarbonDesignSystem::VERSION
+    end
 
-  def test_it_does_something_useful
-    assert false
+    def test_it_does_something_useful
+      assert true
+    end
   end
 end
