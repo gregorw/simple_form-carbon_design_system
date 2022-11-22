a2d3fa8e3f32dd6073a5abe7e0928262d639699d
Gregor Wassmann <gregor.wassmann@gmail.com>
2022-11-22 21:54:07 +0100

    Intitial commit after running `bundle gem …`

---
+++ lib/simple_form/carbon_design_system.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "carbon_design_system/version"
+
+module SimpleForm
+  module CarbonDesignSystem
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/simple_form/carbon_design_system/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module SimpleForm
+  module CarbonDesignSystem
+    VERSION = "0.1.0"
+  end
+end
